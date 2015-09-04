4df1fbed417fc250f1f615b86b6e26add9128137
Johannes Berge <Johannes Berge>
2015-09-04 23:29:55 +0200

    Added Parse!!! noe mangler...

--- Chess♔/AppDelegate.swift
+++ Chess♔/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("hadtED3WDzRnxyboadNlGaBCQQjx569EhHzLs5Fm",
+            clientKey: "iMCLxq9tBCaXHkCYBts5xn94qAgobzDY0Gk68iNr")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
