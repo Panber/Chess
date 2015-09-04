32b3772ab562a303a65d8e9e9e6221b3251d704d
Johannes Berge <Johannes Berge>
2015-09-04 22:45:30 +0200

    j

--- Chess♔/ViewController.swift
+++ Chess♔/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //New line of code
-        //SHA256:+gvt/LKlFC0155sm2lkuVT0WdXU8OtgIMfzgkspPIr4
+        //
     }
 
     override func didReceiveMemoryWarning() {
