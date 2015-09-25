e70b74816841dbfa7a317a4d7117eaca86da1633
AlexPanayotov <alexnaderpanayotov98@gmail.com>
2015-09-25 22:37:38 +0200

    commit

--- Chess♔/FriendsMenu.swift
+++ Chess♔/FriendsMenu.swift
@@ -13,27 +13,24 @@ class FriendsMenu: UIViewController, UITableViewDataSource, UITableViewDelegate,
     
     // Array for users that are being searched for
     var users = NSMutableArray()
+    var filteredUsers = NSMutableArray()
     
-    @IBOutlet weak var searchBar: UISearchBar!
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-     
         
-        // Register costum cell
-//        let nib = UINib(nibName: "Main", bundle: nil)
-//        tableView.registerNib(nib, forCellReuseIdentifier: "cell")
         
     }
-
+    
     // Func that searches for user with key and stores it in an array
-    func searchUsers(search_lower: String) {
+    func searchUsers(search_string: String) {
         
         var query: PFQuery = PFQuery(className:"_User")
         
-        query.whereKey("username", containsString: search_lower)
+        query.whereKey("username", containsString: search_string)
         query.orderByAscending("username")
         query.findObjectsInBackgroundWithBlock{(objects: [AnyObject]?, error: NSError?) -> Void in
             if error == nil {
@@ -55,30 +52,43 @@ class FriendsMenu: UIViewController, UITableViewDataSource, UITableViewDelegate,
     
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
-    // MARK: - Table View
+    
+    // MARK - Table View
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        
+        if tableView == self.searchDisplayController?.searchResultsTableView {
+            
+            return self.filteredUsers.count
+        }
+        else {
+            return users.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         var cell:UserTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! UserTableViewCell
         
-        // Declare user object and set cell text to username
         var user:PFUser = users[indexPath.row] as! PFUser
+        
+        if (tableView == self.searchDisplayController?.searchResultsTableView) {
+            user = self.filteredUsers[indexPath.row] as! PFUser
+        }
+        else {
+            user = self.users[indexPath.row] as! PFUser
+        }
+        
         cell.username.text = user["username"] as! String
         
         let profilePictureObject = user["profile_picture"] as? PFFile
@@ -96,17 +106,30 @@ class FriendsMenu: UIViewController, UITableViewDataSource, UITableViewDelegate,
         }
         
         return cell
+        
     }
     
-    // MARK - Search
-
-    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String?) -> Bool {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
-        if searchString!.characters.count > 0 {
-            self.searchUsers(searchString!)
+        var user:PFUser = users[indexPath.row] as! PFUser
+        
+        if tableView == self.searchDisplayController?.searchResultsTableView {
+            user = self.filteredUsers[indexPath.row] as! PFUser
+        }
+        else {
+            user = self.users[indexPath.row] as! PFUser
         }
         
+        
+    }
+    
+    // MARK: - Search
+    
+    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String?) -> Bool {
+        
+        self.searchUsers(searchString!)
         
         return true
     }
@@ -128,29 +151,29 @@ class FriendsMenu: UIViewController, UITableViewDataSource, UITableViewDelegate,
         if darkMode == true {
             
             
-                self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
-                self.navigationController?.navigationBar.barTintColor = UIColor.darkGrayColor()
-                self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.05, green: 0.05 , blue: 0.05, alpha: 1)
-                
-                self.view.backgroundColor = UIColor(red: 0.15, green: 0.15 , blue: 0.15, alpha: 1)
-                self.tabBarController?.tabBar.barStyle = UIBarStyle.Black
-                self.tabBarController?.tabBar.tintColor = UIColor.whiteColor()
-                self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
-                
-                
+            self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+            self.navigationController?.navigationBar.barTintColor = UIColor.darkGrayColor()
+            self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.05, green: 0.05 , blue: 0.05, alpha: 1)
+            
+            self.view.backgroundColor = UIColor(red: 0.15, green: 0.15 , blue: 0.15, alpha: 1)
+            self.tabBarController?.tabBar.barStyle = UIBarStyle.Black
+            self.tabBarController?.tabBar.tintColor = UIColor.whiteColor()
+            self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+            
+            
             
         }
         else if darkMode == false {
             
-                self.navigationController?.navigationBar.barStyle = UIBarStyle.Default
-                self.navigationController?.navigationBar.barTintColor = UIColor.whiteColor()
-                self.view.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
-                self.tabBarController?.tabBar.barStyle = UIBarStyle.Default
-                self.tabBarController?.tabBar.tintColor = UIColor.blueColor()
-                self.navigationController?.navigationBar.tintColor = UIColor.blueColor()
-                
-                
-                
+            self.navigationController?.navigationBar.barStyle = UIBarStyle.Default
+            self.navigationController?.navigationBar.barTintColor = UIColor.whiteColor()
+            self.view.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
+            self.tabBarController?.tabBar.barStyle = UIBarStyle.Default
+            self.tabBarController?.tabBar.tintColor = UIColor.blueColor()
+            self.navigationController?.navigationBar.tintColor = UIColor.blueColor()
+            
+            
+            
             
         }
         
