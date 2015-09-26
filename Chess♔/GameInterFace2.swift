11ac90bb5d35a183d146e624ae89116707cbe41d
Johannes Berge <Johannes Berge>
2015-09-26 18:15:24 +0200

    j

--- Chess♔/GameInterFace2.swift
+++ Chess♔/GameInterFace2.swift
@@ -6,16 +6,18 @@
 //  Copyright © 2015 Panber. All rights reserved.
 //
 
+
+
 var square = sqrt(screenWidth * screenWidth / 64)
 
 //x-axis
-var a: CGFloat = 0
-var b = square * 2
-var c = square * 3
-var d = square * 4
-var e = square * 5
-var f = square * 6
-var g = square * 7
+var a: CGFloat = square - square
+var b = square
+var c = square * 2
+var d = square * 3
+var e = square * 4
+var f = square * 5
+var g = square * 6
 var h = square * 7
 
 //y-axis
@@ -59,20 +61,21 @@ var wPawn5_id = 5
 var wPawn6_id = 6
 var wPawn7_id = 7
 var wPawn8_id = 8
-var wRook1_id = 9
-var wRook2_id = 10
-var wKnight1_id = 11
-var wKnight2_id = 12
-var wBishop1_id = 13
-var wBishop2_id = 14
-var wQueen1_id = 15
-var wKing1_id = 16
+var wRook1_id = 101
+var wRook2_id = 102
+var wKnight1_id = 201
+var wKnight2_id =  202
+var wBishop1_id = 301
+var wBishop2_id = 302
+var wQueen1_id = 401
+var wKing1_id = 501
 
 var wPieces_ids = [wPawn1_id,wPawn2_id,wPawn3_id,wPawn4_id,wPawn5_id,wPawn6_id,wPawn7_id,wPawn8_id,wRook1_id,wRook2_id,wKnight1_id,wKnight2_id,wBishop1_id,wBishop2_id,wQueen1_id,wKing1_id]
 
-
+var ojId = ""
 
 import UIKit
+import Parse
 
 class GameInterFace2: UIViewController {
 
@@ -81,17 +84,80 @@ class GameInterFace2: UIViewController {
         
         
         //If EXISTING GAME, then load all positions of pieces from the cloud
+        var boardState = PFObject(className: "BoardState4")
+        boardState["wPawn1X"] = 1
+        boardState["wPawn1Y"] = screenHeight/2 + 2 * square
+        boardState["pieceID"] = wPawn1_id
+        boardState.saveInBackgroundWithBlock { (success, error) -> Void in
+            if success {
+                ojId = String(boardState.objectId)
+                print("the oj in the load is \(ojId)")
+                
+            }
+        }
+
         
+        var query = PFQuery(className:"BoardState4")
+        query.getObjectInBackgroundWithId("5IRfGX02gR") {
+            (boardState: PFObject?, error: NSError?) -> Void in
+            if error == nil && boardState != nil {
+                print("the boardstate is \(boardState)")
+                
+                wPawn1.frame.origin.x = CGFloat((boardState?.objectForKey("wPawn1X"))! as! NSNumber)
+                
+            } else {
+                print(error)
+            }
+        }
+            
         
+    
+        print("the oj is\(boardState.objectId)")
+        print(boardState.objectId)
+        print(boardState.objectForKey("wPawn1Y"))
         
         //else is NEW GAME, then load all pieces to view and do additional setup
         for var i = 0; i < wPieces.count; i++ {
-                wPieces[i].userInteractionEnabled = true
-                let _view = wPieces[i]
+            
+            wPieces[i].userInteractionEnabled = true
+            let _view = wPieces[i]
+            
+            if wPieces_ids[i] > 0 && wPieces_ids[i] < 100 {
+                
                 let image = UIImage(named: "whitePawn.png")
                 wPieces[i].image = image
                 view.addSubview(_view)
-        
+            }
+            else if wPieces_ids[i] > 100 && wPieces_ids[i] < 200 {
+                
+                let image = UIImage(named: "whiteRook.png")
+                wPieces[i].image = image
+                view.addSubview(_view)
+            }
+            else if wPieces_ids[i] > 200 && wPieces_ids[i] < 300 {
+                
+                let image = UIImage(named: "whiteKnight.png")
+                wPieces[i].image = image
+                view.addSubview(_view)
+            }
+            else if wPieces_ids[i] > 300 && wPieces_ids[i] < 400 {
+                
+                let image = UIImage(named: "whiteBishop.png")
+                wPieces[i].image = image
+                view.addSubview(_view)
+            }
+            else if wPieces_ids[i] > 400 && wPieces_ids[i] < 500 {
+                
+                let image = UIImage(named: "whiteQueen.png")
+                wPieces[i].image = image
+                view.addSubview(_view)
+            }
+            else if wPieces_ids[i] > 500 && wPieces_ids[i] < 600 {
+                
+                let image = UIImage(named: "whiteKing.png")
+                wPieces[i].image = image
+                view.addSubview(_view)
+            }
         }
 
     }
@@ -103,10 +169,10 @@ class GameInterFace2: UIViewController {
         for var i = 0; i < wPieces.count; i++ {
     
             if touch.view == wPieces[i] {
+                
                 print("touches began!!")
                 movePiece(wPieces[i])
                 return
-                print("1")
             }
             print("2")
     
