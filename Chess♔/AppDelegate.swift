4dead198796c112ed94cfab78431ae8ee7778d80
Johannes Berge <Johannes Berge>
2015-09-30 00:07:20 +0200

    x

--- Chess♔/AppDelegate.swift
+++ Chess♔/AppDelegate.swift
@@ -30,18 +30,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UINavigationControllerDel
         // [Optional] Track statistics around application opens.
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         
-////        check if user has logged in before
-     //   rememrber to delete at logout
-//        let userName:String? = NSUserDefaults.standardUserDefaults().stringForKey("user_name")
-//        
-//        if userName != nil {
-//            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-//            let mainPage = mainStoryboard.instantiateViewControllerWithIdentifier("Sett")
-//            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-//            appDelegate.window?.rootViewController = mainPage
-//
-//        }
-//        
+//        check if user has logged in before
+   //     rememrber to delete at logout
+        let userName:String? = NSUserDefaults.standardUserDefaults().stringForKey("user_name")
+        
+        if userName != nil {
+            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let mainPage = mainStoryboard.instantiateViewControllerWithIdentifier("Sett")
+            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            appDelegate.window?.rootViewController = mainPage
+
+        }
+        
         //notifications
         let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
         application.registerUserNotificationSettings(settings)
