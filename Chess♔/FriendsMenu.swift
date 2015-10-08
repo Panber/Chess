6e32773ee3495d4b4e5e0142c2ccaa3ffd3853c7
AlexPanayotov <alexnaderpanayotov98@gmail.com>
2015-10-08 20:19:34 +0200

    Explore

--- Chess♔/FriendsMenu.swift
+++ Chess♔/FriendsMenu.swift
@@ -169,15 +169,17 @@ class FriendsMenu: UIViewController, UISearchBarDelegate, UISearchDisplayDelegat
     
     // Func that searches for user with key and stores it in an array
     func searchUsers(searchString: String) {
-        
-        self.users.removeAllObjects()
         var query: PFQuery = PFQuery(className:"_User")
         query.whereKey("username", matchesRegex:searchString, modifiers:"i")
         query.orderByAscending("username")
         query.findObjectsInBackgroundWithBlock{(objects: [AnyObject]?, error: NSError?) -> Void in
             if error == nil {
                 for object in objects! {
+                    self.users.removeAllObjects()
                     self.users.addObject(object)
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.tableView.reloadData()
+                    }
                 }
                 print(self.users.count)
                 dispatch_async(dispatch_get_main_queue()) {
