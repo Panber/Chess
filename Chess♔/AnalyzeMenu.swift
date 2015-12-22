32eef05fb109310333e6083f266295e996817f5c
Johannes Berge <Johannes Berge>
2015-12-22 16:12:06 +0100

    fg

--- Chess♔/AnalyzeMenu.swift
+++ Chess♔/AnalyzeMenu.swift
@@ -14,7 +14,12 @@ class AnalyzeMenu: UIViewController,UIScrollViewDelegate {
 
     @IBOutlet weak var board: UIImageView!
   
-
+    var slider = UISlider()
+    var capsuleB = UIButton()
+    var capsuleL = UILabel()
+    
+    var forwardB = UIButton()
+    var backwardB = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +28,139 @@ class AnalyzeMenu: UIViewController,UIScrollViewDelegate {
         navigationController?.navigationBar.topItem?.title = "Analyze"
         self.navigationController!.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Didot", size: 20)!]
 
+        //
+        slider = UISlider(frame:CGRectMake(20, screenHeight/2 + 150, screenWidth - 40, 20))
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.continuous = true
+        slider.tintColor = blue
+        slider.value = 100
+        slider.addTarget(self, action: "sliderValueDidChange:", forControlEvents: .ValueChanged)
+        view.addSubview(slider)
+        view.sendSubviewToBack(slider)
+        //
+        
+        capsuleL = UILabel(frame: CGRectMake(0,screenHeight/2 - 150,screenWidth,60))
+        capsuleL.text = "TIME CAPSULE"
+        capsuleL.font = UIFont(name: "Didot", size: 22)
+        capsuleL.textAlignment = .Center
+        if darkMode {capsuleL.textColor = UIColor.whiteColor()}
+        else {capsuleL.textColor = UIColor.blackColor()}
+        view.addSubview(capsuleL)
+        view.sendSubviewToBack(capsuleL)
+        
+        
+        capsuleB = UIButton(frame: CGRectMake(screenWidth - 60,screenHeight/2 + 246,40,40))
+        capsuleB.setBackgroundImage(UIImage(named: "capsuleClock.png"), forState: .Normal)
+        capsuleB.addTarget(self, action: "capsuleButtonPressed:", forControlEvents: .TouchUpInside)
+        view.addSubview(capsuleB)
+        
+        backwardB = UIButton(frame: CGRectMake(screenWidth/2-70,540 - 52,40,40))
+        backwardB.setBackgroundImage(UIImage(named: "arrow_blueB.png"), forState: .Normal)
+        backwardB.addTarget(self, action: "backwardButtonPressed:", forControlEvents: .TouchUpInside)
+        view.addSubview(backwardB)
+        backwardB.enabled = false
+        view.sendSubviewToBack(backwardB)
+
+        
+        forwardB = UIButton(frame: CGRectMake(screenWidth/2+30,540 - 52,40,40))
+        forwardB.setBackgroundImage(UIImage(named: "arrow_blueF.png"), forState: .Normal)
+        forwardB.addTarget(self, action: "forwardButtonPressed:", forControlEvents: .TouchUpInside)
+        forwardB.enabled = false
+        view.addSubview(forwardB)
+        view.sendSubviewToBack(forwardB)
 
         
     }
 
+    func capsuleButtonPressed(sender: UIButton!) {
+    
+        forwardB.enabled = false
+        backwardB.enabled = true
+
+        
+        UIView.animateWithDuration(0.8, animations: { () -> Void in
+            self.slider.frame.origin.y = 652
+            self.capsuleB.frame.origin.y += 200
+            self.capsuleL.frame.origin.y = 78
+            
+            self.backwardB.frame.origin.y = 600
+            self.forwardB.frame.origin.y = 600
+            
+            }, completion: {finish in
+        
+        })
+        
+    }
+    
+    func sliderValueDidChange(sender:UISlider!)
+    {
+        forwardB.enabled = true
+        
+        if sender.value == 0 {
+            backwardB.enabled = false
+            
+        }
+        else {
+            backwardB.enabled = true
+        }
+        
+        print("value--\(sender.value)")
+        if sender.value == 100 {
+            forwardB.enabled = false
+
+            
+            UIView.animateWithDuration(0.8, animations: { () -> Void in
+                self.slider.frame.origin.y = 540
+                self.capsuleB.frame.origin.y = screenHeight/2 + 246
+                self.capsuleL.frame.origin.y = 200
+                
+                self.backwardB.frame.origin.y = 540 - 52
+                self.forwardB.frame.origin.y = 540 - 52
+
+                }, completion: {finish in
+                    //sender.value = 50
+
+            })
+        
+        }
+        
+    
+    }
+    
+    func forwardButtonPressed(sender:UIButton!) {
+        slider.value++
+        backwardB.enabled = true
+
+        
+        if slider.value == 100 {
+            forwardB.enabled = false
+            
+            UIView.animateWithDuration(0.8, animations: { () -> Void in
+                self.slider.frame.origin.y = 540
+                self.capsuleB.frame.origin.y = screenHeight/2 + 246
+                self.capsuleL.frame.origin.y = 200
+                
+                self.backwardB.frame.origin.y = 540 - 52
+                self.forwardB.frame.origin.y = 540 - 52
+                
+                }, completion: {finish in
+                    //sender.value = 50
+                    
+            })
+
+        }
+    }
+    func backwardButtonPressed(sender:UIButton!) {
+        slider.value--
+        forwardB.enabled = true
+        
+        if slider.value == 0 {
+            backwardB.enabled = false
+            
+        }
+        
+    }
 
     func cancelButtonPressed(sender:UITapGestureRecognizer){
         removeNewView()
